a9852011cc3ab29d18843882c23713046c26ba39
tar_bin <tar.bin.master@gmail.com>
2016-11-23 04:39:08 +0900

    ReactivePropertyでのMVVM実装に変更

--- KMBEditor/MainWindow.xaml.cs
+++ KMBEditor/MainWindow.xaml.cs
@@ -12,9 +12,43 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Reactive.Bindings;
+using Microsoft.Win32;
 
 namespace KMBEditor
 {
+    /// <summary>
+    /// MainWindow の ViewModel
+    /// </summary>
+    public class MainWindowViewModel
+    {
+        // プロパティ
+        public ReactiveProperty<string> AA { get; private set; }
+
+        // コマンド
+        public ReactiveCommand OpenCommand { get; private set; }
+
+        public void OpenMLTFile()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "ファイルを開く";
+            dialog.Filter = "すべてのファイル(*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                this.AA.Value = dialog.FileName;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.AA = new ReactiveProperty<string>("");
+            this.OpenCommand = new ReactiveCommand();
+
+            this.OpenCommand.Subscribe(_ => this.OpenMLTFile());
+        }
+    }
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
