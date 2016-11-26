b786747e419ad98dfb5e819c2ec84a5a64db6ebe
tar_bin <tar.bin.master@gmail.com>
2016-11-27 02:58:22 +0900

    MLTViewerにTreeViewの追加

--- KMBEditor/MLTViewerWindow.xaml.cs
+++ KMBEditor/MLTViewerWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -17,34 +18,33 @@ using WinForms = System.Windows.Forms;
 
 namespace KMBEditor
 {
-    public class Person
-    {
-        public string Name { get; set; }
-        public List<Person> Children { get; set; }
-    }
-
-
     /// <summary>
     /// MLTViewerWindow の ViewModel
     /// </summary>
     public class MLTViewerWindowViewModel
     {
-        public ReactiveProperty<string> resource_path { get; private set; }
+        public ReactiveProperty<string> ResourceDirectoryPath { get; private set; }
+        public ReactiveProperty<List<MLTFileTreeNode>> MLTFileTreeNodes { get; private set; }
 
         public ReactiveCommand OpenResourceDirectoryCommand { get; private set; }
+
 
         public string OpenResourceDirectory()
         {
             using (var dialog = new WinForms.FolderBrowserDialog())
             {
                 dialog.Description = "読み込む対象ディレクトリを指定してください";
-                dialog.SelectedPath = this.resource_path.Value; // 前回選択したディレクトリへのパスを初期値として開始
+                dialog.SelectedPath = this.ResourceDirectoryPath.Value; // 前回選択したディレクトリへのパスを初期値として開始
 
                 // 選択されなかった場合はなにもしない
                 if (dialog.ShowDialog() != WinForms.DialogResult.OK)
                 {
                     return ""; // FIXME: このままだとエラーになるので要修正
                 }
+
+                // 選択されたディレクトリを起点に、MLTファイルを探索
+                var mlt = new MLTFileTreeClass();
+                this.MLTFileTreeNodes.Value = mlt.SearchMLTFile(dialog.SelectedPath);
 
                 // 選択されたディレクトリへのパスを返す
                 return dialog.SelectedPath;
@@ -54,13 +54,37 @@ namespace KMBEditor
         public MLTViewerWindowViewModel()
         {
             // 変数初期化
-            this.resource_path = new ReactiveProperty<string>("");
+            this.ResourceDirectoryPath = new ReactiveProperty<string>("");
+            this.MLTFileTreeNodes = new ReactiveProperty<List<MLTFileTreeNode>>(
+                new List<MLTFileTreeNode> {
+                    new MLTFileTreeNode
+                    {
+                        Name = "test1",
+                        Path = "test1",
+                        Children = new List<MLTFileTreeNode>
+                        {
+                            new MLTFileTreeNode
+                            {
+                                Name = "test2",
+                                Path = "test2",
+                                Children = new List<MLTFileTreeNode>
+                                {
+                                    new MLTFileTreeNode
+                                    {
+                                        Name = "FileName2",
+                                        Path = "FilePath2",
+                                    }
+                                },
+                            }
+                        }
+                    }
+                });
 
             // コマンド初期化
             this.OpenResourceDirectoryCommand = new ReactiveCommand();
 
             // コマンド定義
-            this.OpenResourceDirectoryCommand.Subscribe(_ => this.resource_path.Value = this.OpenResourceDirectory());
+            this.OpenResourceDirectoryCommand.Subscribe(_ => this.ResourceDirectoryPath.Value = this.OpenResourceDirectory());
         }
     }
 
@@ -77,36 +101,6 @@ namespace KMBEditor
         public MLTViewerWindow()
         {
             InitializeComponent();
-
-            this.treeView.ItemsSource = new List<Person>
-            {
-                new Person
-                {
-                    Name = "田中　太郎",
-                    Children = new List<Person>
-                    {
-                        new Person { Name = "田中　花子" },
-                        new Person { Name = "田中　一郎" },
-                        new Person
-                        {
-                            Name = "木村　貫太郎",
-                            Children = new List<Person>
-                            {
-                                new Person { Name = "木村　はな" },
-                                new Person { Name = "木村　梅" },
-                            }
-                        }
-                    }
-                },
-                new Person
-                {
-                    Name = "田中　次郎",
-                    Children = new List<Person>
-                    {
-                        new Person { Name = "田中　三郎" }
-                    }
-                }
-            };
         }
     }
 }
