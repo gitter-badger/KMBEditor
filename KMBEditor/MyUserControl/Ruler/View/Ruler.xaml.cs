999e9bf58a86cf3cc5cb816e586af5f47122044f
tar_bin <tar.bin.master@gmail.com>
2016-12-14 02:55:37 +0900

    ルーラーの表示に対応

---
+++ KMBEditor/MyUserControl/Ruler/View/Ruler.xaml.cs
@@ -0,0 +1,18 @@
+﻿using KMBEditor.MyUserControl.Ruler.ViewModel;
+using System.Windows.Controls;
+
+namespace KMBEditor.MyUserControl.Ruler.View
+{
+    /// <summary>
+    /// Ruler.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Ruler : UserControl
+    {
+        public Ruler()
+        {
+            InitializeComponent();
+
+            this.RulerUserControl.DataContext = new RulerViewModel();
+        }
+    }
+}
---
+++ KMBEditor/MyUserControl/Ruler/ViewModel/Ruler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+using System.Collections.ObjectModel;
+
+namespace KMBEditor.MyUserControl.Ruler.ViewModel
+{
+    public class RulerNumber
+    {
+        public int Number { get; set; }
+    }
+
+    class RulerViewModel
+    {
+        public List<RulerNumber> RulerList { get; private set; } = new List<RulerNumber>();
+
+        public RulerViewModel()
+        {
+            foreach (var i in Enumerable.Range(0,12).Select(x => x * 100))
+            {
+                this.RulerList.Add(new RulerNumber { Number = i });
+            }
+        }
+    }
+}
