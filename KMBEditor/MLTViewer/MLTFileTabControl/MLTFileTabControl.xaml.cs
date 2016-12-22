47ebfd6361b72280e197e76e4254dd5dab7baed4
tar_bin <tar.bin.master@gmail.com>
2016-12-22 17:28:47 +0900

    usingの整理

--- KMBEditor/MLTViewer/MLTFileTabControl/MLTFileTabControl.xaml.cs
+++ KMBEditor/MLTViewer/MLTFileTabControl/MLTFileTabControl.xaml.cs
@@ -2,22 +2,11 @@
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KMBEditor.MLTViewer.MLTFileTabControl
 {
